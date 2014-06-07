169eefd473fcac8065a353a7da95b016b7d7b365
Callum Macrae <callum@macr.ae>
2014-06-07 01:45:45 +0100

    Use absolute paths so it can be ran from anywhere

--- install/install.sh
+++ install/install.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-. ./packages.sh
-. ./git.sh
-. ./zsh.sh
-. ./vim.sh
-. ./node.sh
-. ./irssi.sh
-. ./misc.sh
+# Only define if not already defined
+[[ -z $DOTFILES ]] && export DOTFILES="$HOME/.dotfiles"
+
+. $DOTFILES/install/packages.sh
+. $DOTFILES/install/git.sh
+. $DOTFILES/install/zsh.sh
+. $DOTFILES/install/vim.sh
+. $DOTFILES/install/node.sh
+. $DOTFILES/install/irssi.sh
+. $DOTFILES/install/misc.sh
 
 unset DOTFILES # Don't mess up the PS1
 
