00b822770210c650bd5826bc58cd4f4d9e9ff5e5
Callum Macrae <a-callum.macrae@rvu.co.uk>
2020-03-10 12:52:05 +0000

    Updated vim stuff

--- install/vim.sh
+++ install/vim.sh
@@ -7,3 +7,9 @@
 
 [ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc_old
 ln -s ${DOTFILES}/vim/vimrc ~/.vimrc
+
+[ -f ~/.ideavimrc ] && mv ~/.ideavimrc ~/.ideavimrc_old
+ln -s ${DOTFILES}/vim/ideavimrc ~/.ideavimrc
+
+[ -f ~/.vimrc-common ] && mv ~/.vimrc-common ~/.vimrc-common_old
+ln -s ${DOTFILES}/vim/vimrc-common ~/.vimrc-common
