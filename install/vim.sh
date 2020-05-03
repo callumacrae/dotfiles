32d9d3ee76ba92430eb49cb3cc2d7f2333374b78
Callum Macrae <callum@macr.ae>
2020-05-03 17:16:42 +0100

    Improve nvim install

--- install/vim.sh
+++ install/vim.sh
@@ -17,3 +17,9 @@ ln -s ${DOTFILES}/vim/vimrc-common ~/.vimrc-common
 
 [ -d ~/.config/nvim ] && mv ~/.config/nvim{,_old}
 ln -s ${DOTFILES}/vim/config ~/.config/nvim
+
+# Install vim-plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+nvim +PluginInstall +qall
