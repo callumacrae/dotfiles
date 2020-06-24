239dc00cb28e1ed2e23d3e7fa65037bf22052aa7
Callum Macrae <a-callum.macrae@rvu.co.uk>
2020-06-24 12:10:13 +0100

    Move fzf config into dotfiles

--- install/packages.sh
+++ install/packages.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# Only define if not already defined
+[[ -z $DOTFILES ]] && export DOTFILES="$HOME/.dotfiles"
+
 if [[ -x `which apt-get` ]]; then
 	sudo apt-get update
-	sudo apt-get install -y ack-grep silversearcher-ag autojump htop
+	sudo apt-get install -y ack-grep silversearcher-ag autojump htop fzf
 fi
 
 if [[ `uname` == 'Darwin' ]]; then
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-	brew install ack the_silver_searcher autojump htop wget tree
+	brew install ack the_silver_searcher autojump htop wget tree fzf
 fi
+
+[ -f ~/.fzf.zsh ] && mv ~/.fzf.zsh ~/.fzf.zsh_old
+ln -s ${DOTFILES}/zsh/fzf.zsh ~/.fzf.zsh
