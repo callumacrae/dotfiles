12c7216462a924908a678112a21735a0ef5b6cd2
Callum Macrae <callum@macr.ae>
2022-02-12 14:34:51 +0000

    Show calendar when theres another event today

--- tmux/status/next-event.sh
+++ tmux/status/next-event.sh
@@ -2,6 +2,10 @@
 
 next=($(icalBuddy -nrd -std -n -li 1 -ea -eed -nc -b '' -iep "title,datetime" -po "datetime,title" -ps '| |' -tf "%Y-%m-%d %H:%M" eventsToday))
 
+if [ -z $next ]; then
+	exit 0
+fi
+
 timestampNext=$(date -j -f "%Y-%m-%d %H:%M:%S" "${next[0]} ${next[1]}:00" +%s)
 timestampNow=$(date +%s)
 
@@ -12,4 +16,6 @@ if [ $until -lt 10 ]; then
 fi
 if [ $until -lt 60 ]; then
 	echo " " ${next[@]:1:21}
+else
+	echo " "
 fi
