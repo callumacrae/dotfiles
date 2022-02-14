39cc5e5ce95cea16c4e22af725a95dfa1b436365
Callum Macrae <callum@macr.ae>
2022-02-14 13:44:23 +0000

    fix performance issue in tmux status bar calendar

--- tmux/config/status.tmux
+++ tmux/config/status.tmux
@@ -29,7 +29,7 @@ cpu_load="#(~/.dotfiles/tmux/status/cpu-load.sh)"
 audio_status="#(~/.dotfiles/tmux/status/audio-status.sh)"
 next_event="#(~/.dotfiles/tmux/status/next-event.sh)"
 set -g status-right "${prefix_highlight}#[default]${cpu_load} #[fg=brightwhite,bg=$xgray3] #{pomodoro_status}#{online_status}  #[fg=brightwhite]${audio_status}  ${battery_status}#[fg=brightwhite]${next_event} #[fg=brightwhite,bg=$xgray5] %H:%M | %F "
-set -g status-right-length 80
+set -g status-right-length 90
 
 set -g @batt_color_charge_primary_tier8 brightgreen
 set -g @batt_color_charge_primary_tier7 brightgreen
--- tmux/status/next-event.sh
+++ tmux/status/next-event.sh
@@ -1,9 +1,22 @@
-#/bin/bash
+#!/usr/bin/env bash
+
+cache_file="/tmp/next-event-tmux-cache"
+
+# icalBuddy isn't very CPU friendly so cache until next 10 minutes (HH:M*)
+if [ -f $cache_file ]; then
+  cache_modified=$(date -r $cache_file +"%H:%M")
+  now=$(date +"%H:%M")
+
+  if [[ ${cache_modified:0:4} = ${now:0:4} ]]; then
+  	cat $cache_file
+  	exit 0
+  fi
+fi
 
 next=($(icalBuddy -nrd -std -n -li 1 -ea -eed -nc -b '' -iep "title,datetime" -po "datetime,title" -ps '| |' -tf "%Y-%m-%d %H:%M" eventsToday))
 
 if [ -z $next ]; then
-	exit 0
+  exit 0
 fi
 
 timestampNext=$(date -j -f "%Y-%m-%d %H:%M:%S" "${next[0]} ${next[1]}:00" +%s)
@@ -11,11 +24,14 @@ timestampNow=$(date +%s)
 
 (( until=(timestampNext - timestampNow) / 60 ))
 
-if [ $until -lt 10 ]; then
-	echo "#[fg=yellow]\c"
+if [ $until -lt 0 ]; then
+  color="#[fg=red]"
+elif [ $until -lt 10 ]; then
+  color="#[fg=yellow]"
 fi
+output="$color  "
 if [ $until -lt 60 ]; then
-	echo " " ${next[@]:1:21}
-else
-	echo " "
+  output="$output ${next[@]:1:21}"
 fi
+echo $output
+echo $output > $cache_file
