893ba1b03aa40088d2da1e2e79083005f2d3763b
Callum Macrae <callum@macr.ae>
2022-03-08 10:58:11 +0000

    vim config fixes

--- vim/config/plugin-config/commentary.vim
+++ vim/config/plugin-config/commentary.vim
@@ -1,3 +1,6 @@
 " Support 3gcc
 " https://github.com/tomtom/tcomment_vim/issues/179#issuecomment-603665075
-nmap <expr> gcc v:count? ":<c-u><cr>gc".(v:count1-1)."j" : ":TComment<CR>"
+autocmd VimEnter * nmap <expr> gcc v:count? ":<c-u><cr>gc".(v:count1-1)."j" : ":TComment<CR>"
+
+" Muscle memory from commentary 😬 (and it's easier to type)
+nmap gcap gcip
--- vim/config/plugin-config/hop.lua
+++ vim/config/plugin-config/hop.lua
@@ -1,5 +1,13 @@
-require'hop'.setup()
+require'hop'.setup({
+  case_insensitive = false
+})
 
-vim.api.nvim_set_keymap('', '<leader>fw', "<cmd>HopWord<CR>", { silent = true })
-vim.api.nvim_set_keymap('', '<leader>fe', "<cmd>HopChar1<CR>", { silent = true })
-vim.api.nvim_set_keymap('', '<leader>fr', "<cmd>HopChar2<CR>", { silent = true })
+vim.api.nvim_set_keymap('', '<leader>fw', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })<CR>", { silent = true })
+vim.api.nvim_set_keymap('', '<leader>fW', "<cmd>lua require'hop'.hint_words({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })<CR>", { silent = true })
+vim.api.nvim_set_keymap('', '<leader>fe', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })<CR>", { silent = true })
+vim.api.nvim_set_keymap('', '<leader>fE', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })<CR>", { silent = true })
+vim.api.nvim_set_keymap('', '<leader>fr', "<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR })<CR>", { silent = true })
+vim.api.nvim_set_keymap('', '<leader>fR', "<cmd>lua require'hop'.hint_char2({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR })<CR>", { silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>f*', "yiw<cmd>lua require'hop'.hint_patterns({}, vim.fn.getreg('\"'))<CR>", { silent = true })
+vim.api.nvim_set_keymap('v', '<leader>f*', "y<cmd>lua require'hop'.hint_patterns({}, vim.fn.getreg('\"'))<CR>", { silent = true })
